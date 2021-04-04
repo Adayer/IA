9da2db852c11b90cbf6eaa21e16e28093dbbcd08
Adayer <andres.perez@live.u-tad.com>
2021-04-04 11:38:41 +0200

    Ajustes para video

---
+++ Unity-State-Machine-master/Unity-State-Machine-master/Assets/Example/Scripts/Scream.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scream : MonoBehaviour
+{
+	[SerializeField] private Animator m_cmpAnimator = null; 
+	// Start is called before the first frame update
+    void Start()
+    {
+		m_cmpAnimator.SetTrigger("Idle");
+		print("AAAAAAAAAAAAAAAAAAAAAAH");
+    }
+}
